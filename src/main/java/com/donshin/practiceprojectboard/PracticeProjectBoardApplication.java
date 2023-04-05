48240590afa145e92bad1c2f465bb23c65755df1
Toysin <toysin1203@hotmail.com>
2023-04-05 12:38:21 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 엑츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/com/donshin/practiceprojectboard/PracticeProjectBoardApplication.java
@@ -0,0 +1,13 @@
+package com.donshin.practiceprojectboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticeProjectBoardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticeProjectBoardApplication.class, args);
+    }
+
+}
